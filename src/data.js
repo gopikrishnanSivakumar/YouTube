316d601a046d5a569287645bcd570420f92d3c95
Gopikrishnan Sivakumar <gopikrishmay10@gmail.com>
2024-05-15 23:05:55 +0530

    YouTube-Application

--- src/data.js
+++ src/data.js
@@ -13,5 +13,358 @@ else{
 }
 }
 
+
+
+
+let data ={
+  "kind": "youtube#videoListResponse",
+  "etag": "bp5cj01ioV8GetHTIQjf464EukQ",
+  "items": [
+    {
+      "kind": "youtube#video",
+      "etag": "IU44LflbTQA0Lzxk-7kr2keAlt8",
+      "id": "ajf9eCaGzqk",
+      "snippet": {
+        "publishedAt": "2024-05-13T17:00:03Z",
+        "channelId": "UCxOzbkk0bdVl6-tH1Fcajfg",
+        "title": "LAST TO LEAVE THE PRISON",
+        "description": "In this video the Beta Squad have been sent to prison to see who can survive the longest\n\nSubscribe to everyone! \nAj: @AjShabeel \nSharky: @sharky \nChunkz: @Chunkz \nNiko: @Niko \nKenny: @KingKennyTV \nDarkest Man: @darkestman2128 \n\nFollow us on Social Media to stay connected!\nTwitter ► https://twitter.com/BetaSquad5\nInstagram ►  https://www.instagram.com/betasquad5\nTikTok ► https://www.tiktok.com/@betasquad",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/ajf9eCaGzqk/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/ajf9eCaGzqk/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/ajf9eCaGzqk/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          },
+          "standard": {
+            "url": "https://i.ytimg.com/vi/ajf9eCaGzqk/sddefault.jpg",
+            "width": 640,
+            "height": 480
+          },
+          "maxres": {
+            "url": "https://i.ytimg.com/vi/ajf9eCaGzqk/maxresdefault.jpg",
+            "width": 1280,
+            "height": 720
+          }
+        },
+        "channelTitle": "Beta Squad",
+        "tags": [
+          "Beta Squad",
+          "Last To Leave The Prison",
+          "Last To Leave Prison",
+          "Last To Leave",
+          "Last To Leave Challenge",
+          "Niko Omilana",
+          "Chunkz",
+          "King Kenny",
+          "AJ Shabeel",
+          "Sharky",
+          "Prison Challenge",
+          "Trapped In Prison"
+        ],
+        "categoryId": "24",
+        "liveBroadcastContent": "none",
+        "localized": {
+          "title": "LAST TO LEAVE THE PRISON",
+          "description": "In this video the Beta Squad have been sent to prison to see who can survive the longest\n\nSubscribe to everyone! \nAj: @AjShabeel \nSharky: @sharky \nChunkz: @Chunkz \nNiko: @Niko \nKenny: @KingKennyTV \nDarkest Man: @darkestman2128 \n\nFollow us on Social Media to stay connected!\nTwitter ► https://twitter.com/BetaSquad5\nInstagram ►  https://www.instagram.com/betasquad5\nTikTok ► https://www.tiktok.com/@betasquad"
+        },
+        "defaultAudioLanguage": "en"
+      },
+      "contentDetails": {
+        "duration": "PT1H39M31S",
+        "dimension": "2d",
+        "definition": "hd",
+        "caption": "false",
+        "licensedContent": true,
+        "contentRating": {},
+        "projection": "rectangular"
+      },
+      "statistics": {
+        "viewCount": "2498660",
+        "likeCount": "120991",
+        "favoriteCount": "0",
+        "commentCount": "8220"
+      }
+    },
+    {
+      "kind": "youtube#video",
+      "etag": "w-RgHlldlq2XQCytSE9IICBwrDY",
+      "id": "DQacCB9tDaw",
+      "snippet": {
+        "publishedAt": "2024-05-13T17:45:43Z",
+        "channelId": "UCXZCJLdBC09xxGZ6gcdrc6A",
+        "title": "Introducing GPT-4o",
+        "description": "OpenAI Spring Update – streamed live on Monday, May 13, 2024. \n\nIntroducing GPT-4o, updates to ChatGPT, and more.",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/DQacCB9tDaw/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/DQacCB9tDaw/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/DQacCB9tDaw/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          },
+          "standard": {
+            "url": "https://i.ytimg.com/vi/DQacCB9tDaw/sddefault.jpg",
+            "width": 640,
+            "height": 480
+          },
+          "maxres": {
+            "url": "https://i.ytimg.com/vi/DQacCB9tDaw/maxresdefault.jpg",
+            "width": 1280,
+            "height": 720
+          }
+        },
+        "channelTitle": "OpenAI",
+        "categoryId": "28",
+        "liveBroadcastContent": "none",
+        "defaultLanguage": "en",
+        "localized": {
+          "title": "Introducing GPT-4o",
+          "description": "OpenAI Spring Update – streamed live on Monday, May 13, 2024. \n\nIntroducing GPT-4o, updates to ChatGPT, and more."
+        },
+        "defaultAudioLanguage": "en"
+      },
+      "contentDetails": {
+        "duration": "PT26M13S",
+        "dimension": "2d",
+        "definition": "hd",
+        "caption": "true",
+        "licensedContent": false,
+        "contentRating": {},
+        "projection": "rectangular"
+      },
+      "statistics": {
+        "viewCount": "2261293",
+        "likeCount": "76613",
+        "favoriteCount": "0"
+      }
+    },
+    {
+      "kind": "youtube#video",
+      "etag": "SPYk1wxFhva891g77Cw82ZijIbU",
+      "id": "RU1QyAYa60g",
+      "snippet": {
+        "publishedAt": "2024-05-14T04:24:36Z",
+        "channelId": "UCoYOlthN2PmOIGhmSEcp6oA",
+        "title": "Megalopolis - Teaser Trailer",
+        "description": "Our new film MEGALOPOLIS is the best work I've ever had the privilege to preside over.",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/RU1QyAYa60g/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/RU1QyAYa60g/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/RU1QyAYa60g/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          },
+          "standard": {
+            "url": "https://i.ytimg.com/vi/RU1QyAYa60g/sddefault.jpg",
+            "width": 640,
+            "height": 480
+          },
+          "maxres": {
+            "url": "https://i.ytimg.com/vi/RU1QyAYa60g/maxresdefault.jpg",
+            "width": 1280,
+            "height": 720
+          }
+        },
+        "channelTitle": "Francis Ford Coppola ",
+        "categoryId": "22",
+        "liveBroadcastContent": "none",
+        "localized": {
+          "title": "Megalopolis - Teaser Trailer",
+          "description": "Our new film MEGALOPOLIS is the best work I've ever had the privilege to preside over."
+        }
+      },
+      "contentDetails": {
+        "duration": "PT1M32S",
+        "dimension": "2d",
+        "definition": "hd",
+        "caption": "false",
+        "licensedContent": false,
+        "contentRating": {},
+        "projection": "rectangular"
+      },
+      "statistics": {
+        "viewCount": "603613",
+        "likeCount": "30934",
+        "favoriteCount": "0",
+        "commentCount": "2198"
+      }
+    },
+    {
+      "kind": "youtube#video",
+      "etag": "kDx-vFcm_7XJdPjBYVoFIwlvrCg",
+      "id": "fzcPmPjTGBI",
+      "snippet": {
+        "publishedAt": "2024-05-13T22:09:38Z",
+        "channelId": "UCBODyKF0JMbUd6D9Bklyzbw",
+        "title": "I turned Mario Wonder into a Nuzlocke",
+        "description": "because alpharad can't make a video without putting nuzlockes in the title anymore\n[Sponsored] Download AFK Journey on both PC and mobile for FREE: https://bit.ly/AlpharadNewseason\nCheck out the newest update here: https://activity-afkjourney.farlightgames.com/igamesaijiyyy2404020-activity/\nUse my code: AFKJNEWSEASON to get 500 diamonds and 50,000 coins!\n\n0:00 Intro\n2:56 Sponsor\n3:26 Rest of the video\n\n➤ WATCH LIVE: http://twitch.tv/alpharad\n➤ INSTAGRAM: https://www.instagram.com/alpharad\n➤ DISCORD: http://discord.gg/alpharad\n#supermario #nuzlocke #alpharad\n#afkjourney #rpg #freetoplay #songofstrife\n\nVIDEO EDITOR:\n➤ Dillon: https://twitter.com/Deezus_Walks\n\nFINAL CUT EDITOR: \n➤ me (itsa me)\n\nTHUMBNAIL ART:\n➤ Koro: http://twitter.com/wingedkoro",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/fzcPmPjTGBI/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/fzcPmPjTGBI/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/fzcPmPjTGBI/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          },
+          "standard": {
+            "url": "https://i.ytimg.com/vi/fzcPmPjTGBI/sddefault.jpg",
+            "width": 640,
+            "height": 480
+          },
+          "maxres": {
+            "url": "https://i.ytimg.com/vi/fzcPmPjTGBI/maxresdefault.jpg",
+            "width": 1280,
+            "height": 720
+          }
+        },
+        "channelTitle": "Alpharad",
+        "tags": [
+          "alpharad"
+        ],
+        "categoryId": "20",
+        "liveBroadcastContent": "none",
+        "localized": {
+          "title": "I turned Mario Wonder into a Nuzlocke",
+          "description": "because alpharad can't make a video without putting nuzlockes in the title anymore\n[Sponsored] Download AFK Journey on both PC and mobile for FREE: https://bit.ly/AlpharadNewseason\nCheck out the newest update here: https://activity-afkjourney.farlightgames.com/igamesaijiyyy2404020-activity/\nUse my code: AFKJNEWSEASON to get 500 diamonds and 50,000 coins!\n\n0:00 Intro\n2:56 Sponsor\n3:26 Rest of the video\n\n➤ WATCH LIVE: http://twitch.tv/alpharad\n➤ INSTAGRAM: https://www.instagram.com/alpharad\n➤ DISCORD: http://discord.gg/alpharad\n#supermario #nuzlocke #alpharad\n#afkjourney #rpg #freetoplay #songofstrife\n\nVIDEO EDITOR:\n➤ Dillon: https://twitter.com/Deezus_Walks\n\nFINAL CUT EDITOR: \n➤ me (itsa me)\n\nTHUMBNAIL ART:\n➤ Koro: http://twitter.com/wingedkoro"
+        },
+        "defaultAudioLanguage": "en"
+      },
+      "contentDetails": {
+        "duration": "PT30M55S",
+        "dimension": "2d",
+        "definition": "hd",
+        "caption": "false",
+        "licensedContent": true,
+        "contentRating": {},
+        "projection": "rectangular"
+      },
+      "statistics": {
+        "viewCount": "371304",
+        "likeCount": "23310",
+        "favoriteCount": "0",
+        "commentCount": "1535"
+      }
+    },
+    {
+      "kind": "youtube#video",
+      "etag": "oAJHFfCHFPR9X4VbWQkyZRhtf2I",
+      "id": "YP6RI06J7aM",
+      "snippet": {
+        "publishedAt": "2024-05-13T19:00:06Z",
+        "channelId": "UCtylTUUVIGY_i5afsQYeBZA",
+        "title": "Ian - Figure It Out (Official Music Video)",
+        "description": "Lyrical Lemonade Presents:\nIan - Figure It Out (Official Music Video)\n\nWatch the official Behind the Scenes: https://www.youtube.com/watch?v=Hz6alN5RDXA\n\nStarring: Marshawn Lynch\n\nSong Produced by sxprano\n\nDirected and Edited by AMD\nExecutive Producers: Cole Bennett & Jake Millan\nHead of Production: Krista Worby\nProducer: Joseph Rocha \nDirector of Photography: Cole Graham\nVFX Supervisor: Max Colt\nVFX by Frender\n3D Effects by Reduciano\nColor Grade: AMD \nTitle Design: Daniel Keogh\n\n1st AC: Nick Van-Berkel \n2nd AC: Ethan Stiglich\nBTS: TinyTapes\nPicture Car: Myles Routly \n\n--\n\nLyrical Lemonade is available now at drinklyrical.com 🍋🍑🍉\n\n--\n\nOfficial Channel of Lyrical Lemonade \n\nSubscribe to Lyrical Lemonade on YouTube: https://www.youtube.com/@lyricalemonade\n\nLyrical Lemonade Socials:\n\nhttps://www.instagram.com/lyricalemonade/\nhttps://www.instagram.com/lyricalemonadetwo/\nhttps://www.instagram.com/drinklyrical/\nhttps://www.instagram.com/thesummersmash\nhttps://twitter.com/LyricaLemonade\nhttps://twitter.com/thesummersmash\nhttp://www.facebook.com/lyricalemonade\nhttps://www.tiktok.com/@lyricalemonade?lang=en\n\nAMD Socials:\n\nhttp://www.instagram.com/amdvisuals\nhttp://www.twitter.com/amdvisuals",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/YP6RI06J7aM/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/YP6RI06J7aM/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/YP6RI06J7aM/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          },
+          "standard": {
+            "url": "https://i.ytimg.com/vi/YP6RI06J7aM/sddefault.jpg",
+            "width": 640,
+            "height": 480
+          },
+          "maxres": {
+            "url": "https://i.ytimg.com/vi/YP6RI06J7aM/maxresdefault.jpg",
+            "width": 1280,
+            "height": 720
+          }
+        },
+        "channelTitle": "Lyrical Lemonade",
+        "tags": [
+          "lyrical lemonade",
+          "red truck",
+          "marshawn lynch",
+          "ian ferguson",
+          "ian rapper",
+          "canada",
+          "music video",
+          "train videos",
+          "snow",
+          "canada videos",
+          "ian music video",
+          "amd visuals",
+          "junkyard",
+          "reduciano 3d"
+        ],
+        "categoryId": "24",
+        "liveBroadcastContent": "none",
+        "localized": {
+          "title": "Ian - Figure It Out (Official Music Video)",
+          "description": "Lyrical Lemonade Presents:\nIan - Figure It Out (Official Music Video)\n\nWatch the official Behind the Scenes: https://www.youtube.com/watch?v=Hz6alN5RDXA\n\nStarring: Marshawn Lynch\n\nSong Produced by sxprano\n\nDirected and Edited by AMD\nExecutive Producers: Cole Bennett & Jake Millan\nHead of Production: Krista Worby\nProducer: Joseph Rocha \nDirector of Photography: Cole Graham\nVFX Supervisor: Max Colt\nVFX by Frender\n3D Effects by Reduciano\nColor Grade: AMD \nTitle Design: Daniel Keogh\n\n1st AC: Nick Van-Berkel \n2nd AC: Ethan Stiglich\nBTS: TinyTapes\nPicture Car: Myles Routly \n\n--\n\nLyrical Lemonade is available now at drinklyrical.com 🍋🍑🍉\n\n--\n\nOfficial Channel of Lyrical Lemonade \n\nSubscribe to Lyrical Lemonade on YouTube: https://www.youtube.com/@lyricalemonade\n\nLyrical Lemonade Socials:\n\nhttps://www.instagram.com/lyricalemonade/\nhttps://www.instagram.com/lyricalemonadetwo/\nhttps://www.instagram.com/drinklyrical/\nhttps://www.instagram.com/thesummersmash\nhttps://twitter.com/LyricaLemonade\nhttps://twitter.com/thesummersmash\nhttp://www.facebook.com/lyricalemonade\nhttps://www.tiktok.com/@lyricalemonade?lang=en\n\nAMD Socials:\n\nhttp://www.instagram.com/amdvisuals\nhttp://www.twitter.com/amdvisuals"
+        }
+      },
+      "contentDetails": {
+        "duration": "PT1M51S",
+        "dimension": "2d",
+        "definition": "hd",
+        "caption": "false",
+        "licensedContent": true,
+        "contentRating": {},
+        "projection": "rectangular"
+      },
+      "statistics": {
+        "viewCount": "316498",
+        "likeCount": "37151",
+        "favoriteCount": "0",
+        "commentCount": "2884"
+      }
+    }
+  ],
+  "nextPageToken": "CAUQAA",
+  "pageInfo": {
+    "totalResults": 200,
+    "resultsPerPage": 5
+  }
+}
+
+
+
   
 
